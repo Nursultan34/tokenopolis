7d0caac7e8cce590f5d3e277324f3dc131466c41
Andrew <andrew.lus@tuta.io>
2023-03-19 22:00:36 +0100

    Cleanup CalendarEvent.tsx

--- islands/CalendarEvent.tsx
+++ islands/CalendarEvent.tsx
@@ -1,8 +1,5 @@
-/* import { CalendarEventObject } from "../components/DashBoardComponent/CalendarComponent.tsx"; */
 
-/* function CalendarEvent(props: CalendarEventObject) { */
-function CalendarEvent(props) {
-	const { date, time, desc } = props;
+function CalendarEvent({ date, time, desc }) {
 	return (
 		<div class="w-full h-[45%] bg-gray-white mt-2 items-center flex flex-col justify-center items-center pl-3 pr-3">
 			<div class="flex flex-1 justify-between w-full items-center">
