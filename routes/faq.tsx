b2d173213b23a157d034c8b57aa9a77c0466fd6e
Ivan <105598235+JosefKru@users.noreply.github.com>
2023-03-23 02:47:04 +0300

    Make the FAQ page adaptive

--- routes/faq.tsx
+++ routes/faq.tsx
@@ -1,153 +1,150 @@
-import { asset } from "$fresh/runtime.ts";
+import { asset } from '$fresh/runtime.ts'
+import screenWrapper from '@/lib/screenWrapper.tsx'
 
 export default function Faq() {
-	return (
-		<article class="bg(gray-fon) row w-full h-full text(black) py-[42px] pl-[60px]">
-			<FirstSection />
-			<div class="col gap-6">
-				<SecondSection />
-				<ThirdSection />
-			</div>
-		</article>
-	);
+  return screenWrapper(
+    <article class='row gap-5 w-full h-full text(black) py-10 pl-[60px]'>
+      <FirstSection />
+      <div class='col gap-6 h-full w-[43%]'>
+        <SecondSection />
+        <ThirdSection />
+      </div>
+    </article>
+  )
 }
 
 function FirstSection() {
-	const questions = [
-		{
-			title: "Что такое токены и для чего они нужны?",
-			answer: "",
-		},
-		{
-			title: "Как инвестировать через личный кабинет?",
-			answer: "",
-		},
-		{
-			title: "Как выбрать подходящий объект?",
-			answer: "",
-		},
-		{
-			title: "Что такое токены и для чего они нужны?",
-			answer: "",
-		},
-		{
-			title: "Что такое токены и для чего они нужны?",
-			answer: "",
-		},
-		{
-			title: "Что такое токены и для чего они нужны?",
-			answer: "",
-		},
-		{
-			title: "Что такое токены и для чего они нужны?",
-			answer: "",
-		},
-		{
-			title: "Что такое токены и для чего они нужны?",
-			answer: "",
-		},
-		{
-			title: "Что такое токены и для чего они нужны?",
-			answer: "",
-		},
-		{
-			title: "Что такое токены и для чего они нужны?",
-			answer: "",
-		},
-		{
-			title: "Что такое токены и для чего они нужны?",
-			answer: "",
-		},
-	];
-	return (
-		<div class="bg(white) w-[728px] h-[898px] rounded mr-[20px] shadow-lg shadow-black/15">
-			<h2 class="font-bold text-2xl pt-10 pl-9">Частые вопросы</h2>
-			<div class="col items-start w-[702px] mt-2 ml-[26px] ">
-				<div class="col items-start gap-3 w-[692px] h-[790px] overflow-y-scroll p-[10px]">
-					{questions.map((question) => (
-						<div class="bg(gray-white) w-[654px] h-[76px] rounded">
-							<div class="row justify-between p-7 pr-8">
-								<h3 class="font-bold text-lg">{question.title}</h3>
-								<img src={asset("cross.svg")} class="w-[20px]" />
-							</div>
-						</div>
-					))}
-				</div>
-			</div>
-		</div>
-	);
+  const questions = [
+    {
+      title: 'Что такое токены и для чего они нужны?',
+      answer: '',
+    },
+    {
+      title: 'Как инвестировать через личный кабинет?',
+      answer: '',
+    },
+    {
+      title: 'Как выбрать подходящий объект?',
+      answer: '',
+    },
+    {
+      title: 'Что такое токены и для чего они нужны?',
+      answer: '',
+    },
+    {
+      title: 'Что такое токены и для чего они нужны?',
+      answer: '',
+    },
+    {
+      title: 'Что такое токены и для чего они нужны?',
+      answer: '',
+    },
+    {
+      title: 'Что такое токены и для чего они нужны?',
+      answer: '',
+    },
+    {
+      title: 'Что такое токены и для чего они нужны?',
+      answer: '',
+    },
+    {
+      title: 'Что такое токены и для чего они нужны?',
+      answer: '',
+    },
+    {
+      title: 'Что такое токены и для чего они нужны?',
+      answer: '',
+    },
+    {
+      title: 'Что такое токены и для чего они нужны?',
+      answer: '',
+    },
+  ]
+  return (
+    <div class='bg(white-dark) w-[43%] h-full rounded shadow-lg shadow-black/15'>
+      <h2 class='font-bold text-2xl pt-10 pl-9'>Частые вопросы</h2>
+      <div class='col items-start mt-2 ml-6 h-full'>
+        <div class='col items-start gap-3 w-full h-[88%] overflow-y-scroll p-2.5'>
+          {questions.map((question) => (
+            <div class='bg(gray-light) w-[95%] rounded'>
+              <div class='row justify-between p-7 pr-8'>
+                <h3 class='font-bold text-lg'>{question.title}</h3>
+                <img src={asset('cross.svg')} class='w-[20px]' />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
 }
 
 function SecondSection() {
-	return (
-		<div class="bg(white) w-[732px] h-[270px] rounded relative shadow-lg shadow-black/15">
-			<div class="w-[517px] h-[190px] py-10 pl-9 text-lg">
-				<p class="mb-[17px]">
-					Мы собрали большинство вопросов, которые задают наши клиенты. Но если вы не нашли ответ на свой вопрос, напишите нам по адресу:{" "}
-					<span class="font-bold underline">info@tokenopolis.biz</span>
-				</p>
-				<p class="mb-[11px]">Либо обратитесь к вашему менеджеру</p>
-				<button class="w-[240px] h-[48px] px-[20px] py-[15px] bg(yellow-btn) rounded-lg text-sm uppercase">
-					задать вопрос
-				</button>
-			</div>
-			<img
-				src={asset("contact-vector.svg")}
-				class="w-[183px] h-[190px] absolute right-9 top-10"
-			/>
-		</div>
-	);
+  return (
+    <div class='bg(white-dark) w-full h-full rounded relative shadow-lg shadow-black/15'>
+      <div class='w-[75%] h-48 py-10 pl-9 text-lg relative z-10'>
+        <p class='mb-4'>
+          Мы собрали большинство вопросов, которые задают наши клиенты. Но если
+          вы не нашли ответ на свой вопрос, напишите нам по адресу:{' '}
+          <span class='font-bold underline'>info@tokenopolis.biz</span>
+        </p>
+        <p class='mb-2.5'>Либо обратитесь к вашему менеджеру</p>
+        <button class='w-60 h-12 px-5 py-4 bg(yellow-orange) rounded-lg text-sm uppercase focus:outline-none'>
+          задать вопрос
+        </button>
+      </div>
+      <img
+        src={asset('contact-vector.svg')}
+        class='w-[25%] absolute right-9 top-10'
+      />
+    </div>
+  )
 }
 
 function ThirdSection() {
-	const articles = [
-		{
-			title: "Что такое токены? для чего они нужны и какие токены бывают?",
-			img: "plug.png",
-			href: "",
-		},
-		{
-			title: "Как инвестировать через личный кабинет?",
-			img: "plug.png",
-			href: "",
-		},
-		{
-			title: "Как инвестировать через личный кабинет?",
-			img: "plug.png",
-			href: "",
-		},
-		{
-			title: "Как выбрать подходящий объект?",
-			img: "plug.png",
-			href: "",
-		},
-		{
-			title: "Что такое токены? для чего они нужны и какие токены бывают?",
-			img: "plug.png",
-			href: "",
-		},
-	];
-	return (
-		<div class="bg(white) w-[732px] h-[604px] shadow-lg shadow-black/15">
-			<div class="w-[668px] h-[520px] pt-10 pl-[28px] pb-[44px] pr-9">
-				<h2 class="text-2xl font-bold mb-[22px]">Обучающие статьи</h2>
-				<div class="w-[668px] h-[472px] col gap-2 overflow-y-scroll">
-					{articles.map((article) => (
-						<a href={article.href}>
-							<div class="w-[634px] h-[112px] border rounded border-[#D1D1D1] row items-center justify-start">
-								<img
-									src={asset(article.img)}
-									class="px-2 pl-2 pr-[20px]"
-								/>
-								<p class="w-[353px] h-[44px] uppercase mr-[56px]">
-									{article.title}
-								</p>
-								<img src={asset("arrow-article.svg")} />
-							</div>
-						</a>
-					))}
-				</div>
-			</div>
-		</div>
-	);
+  const articles = [
+    {
+      title: 'Что такое токены? для чего они нужны и какие токены бывают?',
+      img: 'plug.png',
+      href: '',
+    },
+    {
+      title: 'Как инвестировать через личный кабинет?',
+      img: 'plug.png',
+      href: '',
+    },
+    {
+      title: 'Как инвестировать через личный кабинет?',
+      img: 'plug.png',
+      href: '',
+    },
+    {
+      title: 'Как выбрать подходящий объект?',
+      img: 'plug.png',
+      href: '',
+    },
+    {
+      title: 'Что такое токены? для чего они нужны и какие токены бывают?',
+      img: 'plug.png',
+      href: '',
+    },
+  ]
+  return (
+    <div class='bg(white-dark) w-full h-[66%] shadow-lg shadow-black/15'>
+      <div class='w-full h-full pt-10 pl-7 pb-11 pr-9'>
+        <h2 class='text-2xl font-bold mb-6'>Обучающие статьи</h2>
+        <div class='w-full h-[93%] col gap-2 overflow-y-scroll'>
+          {articles.map((article) => (
+            <a href={article.href}>
+              <div class='w-[93%] h-[118%] border rounded border-[#D1D1D1] row items-center justify-start'>
+                <img src={asset(article.img)} class='px-2 pl-2 pr-5' />
+                <p class='w-[56%] uppercase mr-14'>{article.title}</p>
+                <img src={asset('arrow-article.svg')} />
+              </div>
+            </a>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
 }
--- routes/manager.tsx
+++ routes/manager.tsx
@@ -1,140 +1,147 @@
-import { asset } from "$fresh/runtime.ts";
-import { tw } from "twind";
+import { asset } from '$fresh/runtime.ts'
+import screenWrapper from '@/lib/screenWrapper.tsx'
 
 export default function Manager() {
-	return (
-		<article class="flex gap-5 bg(gray-fon) w-full h-full text-black pb-[44px]">
-			<div class="col gap-[20px] w-[732px] h-[894px] ml-[60px] mt-10">
-				<FirstSection />
-				<SecondSection />
-			</div>
-			<div class="col gap-[20px] mt-10">
-				<ThirdSection />
-				<FourthSection />
-			</div>
-		</article>
-	);
+  return screenWrapper(
+    <article class='flex gap-5 bg(gray-light) w-full h-full text-black pb-11'>
+      <div class='col gap-5 w-[43%] h-[894px] ml-16 mt-10'>
+        <FirstSection />
+        <SecondSection />
+      </div>
+      <div class='col gap-5 mt-10 w-[42%]'>
+        <ThirdSection />
+        <FourthSection />
+      </div>
+    </article>
+  )
 }
 
 function FirstSection() {
-	return (
-		<div class="bg(white) w-[732px] h-[578px] rounded-sm shadow-lg shadow-black/15 py-10 px-[60px]">
-			<div class="col gap-6 w-[619px] h-[490px] ">
-				<div class="row w-[619px] h-[359px] gap-6">
-					<div class="w-[280px] h-[359px] rounded-sm">
-						<img src={asset("manager-ava.png")} class="object-contain" />
-					</div>
+  return (
+    <div class='bg(white-dark) w-[98%] h-[65%] rounded-sm shadow-lg shadow-black/15  py-10 px-14'>
+      <div class='col gap-6 w-full h-full'>
+        <div class='row w-[88%] h-[60%] gap-6'>
+          <div class='w-72 rounded-sm'>
+            <img src={asset('manager-ava.png')} class='object-contain' />
+          </div>
 
-					<div class="col items-start mt-[145px]">
-						<div class="col w-[297px] h-[105px] gap-3">
-							<h1 class="text-4xl font-bold">Сергей Ольховский</h1>
-							<p class="text-lg">Менеджер</p>
-						</div>
+          <div class='col items-start mt-36'>
+            <div class='col w-72 h-24 gap-3'>
+              <h1 class='text-4xl font-bold'>Сергей Ольховский</h1>
+              <p class='text-lg'>Менеджер</p>
+            </div>
 
-						<div class="flex justify-end items-start w-[188px] h-[60px] gap-2 mt-[50px]">
-							<div class="bg(green-btn) w-[90px] h-[60px] flex justify-center items-center rounded-lg">
-								<img src={asset("phone.svg")} />
-							</div>
-							<div class="bg(blue-btn) w-[90px] h-[60px] flex justify-center items-center rounded-lg">
-								<img src={asset("telegram.svg")} />
-							</div>
-						</div>
-					</div>
-				</div>
+            <div class='flex justify-end items-start w-48 h-14 gap-2 mt-12'>
+              <div class='bg(green-btn) w-24 h-16 flex justify-center items-center rounded-lg'>
+                <img src={asset('phone.svg')} />
+              </div>
+              <div class='bg(blue-btn) w-24 h-16 flex justify-center items-center rounded-lg'>
+                <img src={asset('telegram.svg')} />
+              </div>
+            </div>
+          </div>
+        </div>
 
-				<div class="w-[601px] h-[105px] text-lg ">
-					<p>
-						На рынке недвижимости Черногории с 2010 года. Курирует строительство объектов недвижимости страны, сопровождает сделки купли-продажи.
-					</p>
-				</div>
-			</div>
-		</div>
-	);
+        <div class='w-[84%] text-lg '>
+          <p>
+            На рынке недвижимости Черногории с 2017 года. Курирует строительство
+            объектов недвижимости страны, сопровождает сделки купли-продажи.
+            Даст компетентную консультацию по любым вопросам инвестиций в
+            криптовалюте.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 function SecondSection() {
-	return (
-		<div class="w-[728px] h-[300px] bg(white) rounded-sm shadow-lg shadow-black/15 relative">
-			<div class="w-[454px] h-[166px] pt-[44px] pl-[60px]">
-				<h2 class="text-2xl font-bold mb-[10px]">Контакты</h2>
-				<div class="w-[454px] h-[130px] row gap-[15px] uppercase text-sm leading-[160%]">
-					<div class="w-[166px] h-[105px] ">
-						<p>Телефон:</p>
-						<p>Электронная почта:</p>
-						<p>Адрес:</p>
-					</div>
-					<div class="w-[273px] h-[130px]">
-						<p>123-456-7890</p>
-						<p>info@tokenopolis.biz</p>
-						<p>Черногория,</p>
-						<p>г. Тиват,</p>
-						<p>ул. Строителей, 8</p>
-					</div>
-				</div>
-			</div>
-			<img
-				src={asset("contact-vector.svg")}
-				class="w-[204px] h-[212px] absolute right-[68px] top-[44px]"
-			/>
-		</div>
-	);
+  return (
+    <div class='w-[98%] h-[34%] bg(white-dark) rounded-sm shadow-lg shadow-black/15 relative'>
+      <div class='w-[65%] pt-11 pl-16'>
+        <h2 class='text-2xl font-bold mb-2.5'>Контакты</h2>
+        <div class='w-full row gap-3.5 uppercase text-sm leading-[160%]'>
+          <div class='w-40 h-28'>
+            <p>Телефон:</p>
+            <p>Электронная почта:</p>
+            <p>Адрес:</p>
+          </div>
+          <div class='w-72 h-32'>
+            <p>123-456-7890</p>
+            <p>info@tokenopolis.biz</p>
+            <p>Черногория,</p>
+            <p>г. Тиват,</p>
+            <p>ул. Строителей, 8</p>
+          </div>
+        </div>
+      </div>
+      <img
+        src={asset('contact-vector.svg')}
+        class='w-52 h-52 absolute right-16 top-11'
+      />
+    </div>
+  )
 }
 
 function ThirdSection() {
-	return (
-		<div class="bg(white) w-[732px] h-[296px] rounded-sm shadow-lg shadow-black/15 px-9 py-10">
-			<div class="col justify-center items-start gap-4">
-				<div class="text-lg w-[644px] h-[120px]">
-					<p>
-						Мы собрали большинство вопросов, которые задают наши клиенты на странице FAQ.
-					</p>
-					<p>
-						Прежде, чем писать менеджеру, проверьте, нет ли ответа на интересующий вас вопрос на странице с часто задаваемыми вопросами.
-					</p>
-				</div>
-				<button class="bg(yellow-btn) w-72 h-14 text-4xl flex justify-center items-center rounded-lg px-5 py-4">
-					FAQ
-				</button>
-			</div>
-		</div>
-	);
+  return (
+    <div class='bg(white-dark) w-full h-[33%] rounded-sm shadow-lg shadow-black/15 '>
+      <div class='col justify-center items-start gap-4 mx-9 my-12'>
+        <div class='text-lg w-[94%]'>
+          <p>
+            Мы собрали большинство вопросов, которые задают наши клиенты на
+            странице FAQ.
+          </p>
+          <p>
+            Скорее всего ответ на интересующий вопрос уже подробно описан,
+            проверьте, пожалуйста.
+          </p>
+        </div>
+        <button class='border border(yellow-orange) w-72 h-14 text-4xl flex justify-center items-center rounded-lg px-5 py-4'>
+          FAQ
+        </button>
+      </div>
+    </div>
+  )
 }
 
 function FourthSection() {
-	return (
-		<div class="w-[732px] h-[580px] bg(white) rounded-sm shadow-lg shadow-black/15">
-			<div class="col gap-[20px] w-[657px] h-[192px] ml-9 mr-[39px] mt-[28px] mb-[11px]">
-				<h2 class="text-2xl font-bold">О компании</h2>
-				<p>
-					Мы стремимся обеспечить нашим клиентам выгодное и безопасное инвестирование с помощью использования современных технологий и прозрачной системы учета.
-				</p>
-				<p>
-					Максимально учитываем интересы и пожелания наших инвесторов, чтобы сделать совместную работу максимально выгодной и продуктивной.
-				</p>
-			</div>
+  return (
+    <div class='w-full h-[65%] bg(white-dark) rounded-sm shadow-lg shadow-black/15'>
+      <div class='col gap-5 w-[88%] h-[33%] ml-9 mr-[39px] mt-[28px] mb-[11px]'>
+        <h2 class='text-2xl font-bold'>О компании</h2>
+        <p>
+          Мы стремимся обеспечить нашим клиентам выгодное и безопасное
+          инвестирование с помощью использования современных технологий и
+          прозрачной системы учета.
+        </p>
+        <p>
+          Максимально учитываем интересы и пожелания наших инвесторов, чтобы
+          сделать совместную работу максимально выгодной и продуктивной.
+        </p>
+      </div>
 
-			<h3 class="text-lg font-bold ml-[36px] mb-[20px]">
-				Направление деятельности
-			</h3>
-			<div class="flex items-center justify-center">
-				<div class="w-[40px] h-[80px] flex items-center justify-center">
-					<img src={asset("arrow-left.svg")} />
-				</div>
+      <h3 class='text-lg font-bold ml-9 mb-5'>Направление деятельности</h3>
+      <div class='flex items-center justify-center'>
+        <div class='w-10 h-20 flex items-center justify-center'>
+          <img src={asset('arrow-left.svg')} />
+        </div>
 
-				<div class="flex w-[508px] h-[273px] border border-[#B8B8B7] rounded">
-					<div class="w-[198px] h-[189px] rounded mx-[24px] my-[42px]">
-						<img src={asset("rectangle.png")} class="object-contain" />
-					</div>
-					<p class="w-[237px] h-[157px] mt-[42px] mr-[25px]">
-						Инфраструктурные проекты. Работы по прокладке инженерных коммуникаций и сетей (свет, водопровод, канализация, дороги), строительство и монтаж септиков,
-						ливнёвок.
-					</p>
-				</div>
+        <div class='flex w-[69%] h-72 border border-[#B8B8B7] rounded'>
+          <div class='w-48 h-48 rounded mx-6 my-11'>
+            <img src={asset('rectangle.png')} class='object-contain' />
+          </div>
+          <p class='w-72 h-40 mt-11 mr-6'>
+            Инфраструктурные проекты. Работы по прокладке инженерных
+            коммуникаций и сетей (свет, водопровод, канализация, дороги),
+            строительство и монтаж септиков, ливнёвок.
+          </p>
+        </div>
 
-				<div class="w-[40px] h-[80px] flex items-center justify-center">
-					<img src={asset("arrow-right.svg")} />
-				</div>
-			</div>
-		</div>
-	);
+        <div class='w-10 h-20 flex items-center justify-center'>
+          <img src={asset('arrow-right.svg')} />
+        </div>
+      </div>
+    </div>
+  )
 }
