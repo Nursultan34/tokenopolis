b8926009282d96cf9467f376fa17594c15b803fd
NataliaVozhdaeva <LomzNat@yandex.ru>
2023-04-06 15:16:34 +0200

    Transactions integration + page changes

--- islands/Transaction.tsx
+++ islands/Transaction.tsx
@@ -1,65 +1,82 @@
 import { asset } from "$fresh/runtime.ts";
 import { boolState } from "#/utils.ts";
+import { useEffect } from "preact/hooks";
 
-export default function Transaction({ from, to }) {
+export default function Transactions({ operations }) {
+	return operations.map(Transaction);
+}
+
+function Transaction({ target, isIncoming, amount, asset_code, created_at }) {
 	const [showDetails, toggleDetails] = boolState();
-	// TODO: deprecate
-	const status = "reject";
+	// TODO: deprecate status
+	const status = "success";
 
 	return (
 		<div class="relative w-full">
 			<div
-				class={`flex px-[32px] py-[14px] justify-between bg-white shadow-lg shadow-black/15 hover:(cursor-pointer bg-[#F4F4F4]) ${showDetails ? "bg-[#F4F4F4]" : ""}`}
+				class={`flex lg:px-11 lg:py-[14px] px-3 py-4 justify-between bg-white shadow-lg shadow-black/15 hover:(cursor-pointer bg-[#F4F4F4]) ${showDetails ? "bg-[#F4F4F4]" : ""}`}
 				onClick={toggleDetails}
 			>
-				<div class="flex gap-x-[60px] items-center text-[18px]">
-					<span class="font-bold font-open-sans">12 дек 2018</span>
+				<div class="flex lg:gap-x-[70px] items-center lg:text-[18px] text-[10px] gap-x-4">
+					<span class="font-bold font-open-sans">{ created_at }</span>
 
 					<div class="bg-gray-white">
-						<img src={asset("/./lk-logo.svg")} class="" />
+						<img src={asset("/lk-logo.svg")} class="w-10 lg:w-[55px]" />
 					</div>
 
 					<div class="flex gap-x-2">
-						<img src={asset("/./transaction-arrow.svg")} />
-						1Cs4wu6pu5qCZ35bSLNVzGyEx5N6uzbg9t
+						<img src={asset(isIncoming ? "/incoming-arrow.svg" : "/outcoming-arrow.svg")} />
+						<span class="hidden lg:inline-block">{ target }</span>
 					</div>
 				</div>
-				<div class="flex gap-x-[120px] items-center ">
-					<span class="text-[26px] font-bold font-open-sans">0,0094 LTC</span>
-					<button
-						class={`flex justify-between w-40 max-h-10 px-5 py-2 text-left ${
-							status === "waiting" ? "text-gray-main bg-gray-waiting" : status === "reject" ? "text-white bg-red-3" : "text-white bg-green-3"
+				<div class="flex lg:gap-x-[120px] gap-x-5 items-center ">
+					<span class="lg:text-[26px] text-[10px] font-bold font-open-sans">{amount} {asset_code}</span>
+				
+			<button
+						class={`flex justify-between lg:w-40 w-15 max-h-10 px-5 py-2 text-left ${
+							status === "waiting" ? "text-gray-dark bg-gray-cool" : status === "reject" ? "bg-[#CD1000] text-white-light" : "bg-green-dark text-white-light"
 						} rounded-sm`}
 					>
-						{status === "waiting" ? "В ожидании" : status === "reject" ? "Отклонено" : "Завершено"}
+						 <span class="hidden lg:inline-block"> {status === "waiting" ? "В ожидании" : status === "reject" ? "Отклонено" : "Завершено"}</span> 
 						<img
 							src={asset(
 								`./${status === "waiting" ? "btn-waiting" : status === "reject" ? "btn-reject" : "btn-ok"}.svg`,
 							)}
 						/>
-					</button>
+					</button> 
 				</div>
 			</div>
 
 			{showDetails && (
-				<div class="w-full flex px-[32px] py-[14px] bg-[#F4F4F4] justify-between shadow-lg shadow-black/15 hover:(cursor-pointer bg-[#F4F4F4])">
-					<div class="w-[1180px] flex flex-wrap gap-[20px] ">
-						<label class="flex self-start text-[18px] font-bold font-open-sans">
+				<div class="relative w-full lg:h-40 flex lg:px-11 px-3 lg:py-3 pt-4 pb-5 bg-[#F4F4F4] justify-between shadow-lg shadow-black/15 hover:(cursor-pointer bg-[#F4F4F4])">
+					<div class="w-[1180px] flex lg:flex-wrap flex-col  gap-y-2 lg:max-h-40">
+						<label class="flex items-center w-[180px] lg:w-[360px] justify-between lg:text-[18px] text-[10px] font-bold font-open-sans">
 							Статус:
 							<input
 								type="text"
 								value="Успешно"
-								class="w-[144] ml-5 px-6 py-2.5 text-[18px] bg-white border border-gray-waiting"
+								class="w-36 lg:w-[280px] lg:px-6 lg:py-2.5 px-2.5 py-2 lg:text-[18px] text-[10px] bg-white border border-gray-cool rounded-sm"
 							/>
 						</label>
-						<label class="flex self-start text-[18px] font-bold font-open-sans">
+						<label class="flex items-center w-[180px] lg:w-[360px] justify-between lg:text-[18px] text-[10px] font-bold font-open-sans">
+							Время:
+							<input
+								type="text"
+								value="12 дек 2018 05:37:54"
+								class="w-36 lg:w-[280px] lg:px-6 lg:py-2.5 px-2.5 py-2 lg:text-[18px] text-[10px] bg-white border border-gray-cool rounded-sm"
+							/>
+						</label>
+						<div class="flex">
+						<label class="flex w-[266px] lg:w-[700px] justify-between items-center lg:text-[18px] text-[10px] font-bold font-open-sans ">
 							Хэш транзакции:
 							<input
 								type="text"
 								value="jbfkjbj4453vvfvmbajhnm564ybjldg9rkjkj"
-								class="w-[525px] ml-5 px-6 py-2.5 text-[18px] bg-white border border-gray-waiting"
+								class="lg:w-[525px] w-[182px] lg:px-6 lg:py-2.5 px-2.5 py-2 lg:text-[18px] text-[10px] bg-white border border-gray-cool rounded-sm"
 							/>
-							<img
+							
+						</label>
+						<img
 								class="self-center ml-3 w-7 h-7 hover:(cursor-pointer)"
 								src={asset("/./documentcopy.svg")}
 								{
@@ -74,25 +91,17 @@ export default function Transaction({ from, to }) {
 										   }} */
 								}
 							/>
-						</label>
-						<label class="flex self-start text-[18px] font-bold font-open-sans">
-							Время:
-							<input
-								type="text"
-								value="12 дек 2018 05:37:54"
-								class="w-[144] ml-5 px-6 py-2.5 text-[18px] bg-white border border-gray-waiting"
-							/>
-						</label>
-						<label class="flex self-start text-[18px] font-bold font-open-sans">
+						</div>
+						<label class="flex lg:w-[475px] w-[266px] justify-between items-center lg:text-[18px] text-[10px] font-bold font-open-sans">
 							Комиссия сети:
 							<input
 								type="text"
 								value="0.1563758234 LTC"
-								class="w-[300px] ml-8 px-6 py-2.5 text-[18px] bg-white border border-gray-waiting"
+								class="lg:w-[300px] w-[182px] lg:px-6 lg:py-2.5 px-2.5 py-2 lg:text-[18px] text-[10px] bg-white border border-gray-cool rounded-sm"
 							/>
 						</label>
 					</div>
-					<button class="w-40 text-black text-center bg-gray-waiting">ПОСМОТРЕТЬ НА ЭКСПЛОРЕРЕ</button>
+					<button class="absolute top-4 right-3 lg:right-11 lg:h-[5.5rem] h-[4.5rem] lg:w-40 w-28 px-5 py-2 text-black text-center lg:text-[14px] text-[10px] bg-gray-cool rounded-sm">ПОСМОТРЕТЬ НА ЭКСПЛОРЕРЕ</button>
 				</div>
 			)}
 			{
--- routes/transactions.tsx
+++ routes/transactions.tsx
@@ -1,5 +1,5 @@
 import { asset } from "$fresh/runtime.ts";
-import Transaction from "@/islands/Transaction.tsx";
+import TransactionsI from "@/islands/Transaction.tsx";
 import dashboardScreen from "#/screenWrapper.tsx";
 
 import { Handlers } from "$fresh/server.ts";
@@ -20,46 +20,58 @@ export const handler: Handlers = {
 		// Get the transactions the account is involved in
 		const transactions = await stellar.getTransactions(await stellar.server.loadAccount(keypair.publicKey()), 0);
 		const _operations = await Promise.all(transactions.map(tx => tx.operations()));
+		const infer_is_incoming = o => ({
+			target: o.from === keypair.publicKey() ? o.to : o.from,
+			isIncoming: o.from !== keypair.publicKey(),
+			...o
+		});
 		const operations = R.pipe(
 			R.map(R.prop("records")),
 			R.flatten,
 			R.filter(R.propEq("type", "payment")),
-			R.map(R.pick(["type", "from", "to", "amount", "asset_code", "asset_issuer", "transaction_hash"])),
-			R.over(R.lensProp("asset_code"), R.defaultTo("XLM")),
+			R.map(infer_is_incoming),
+			R.map(R.over(R.lensProp("asset_code"), R.defaultTo("XLM"))),
+			R.map(R.pick(["asset_code", "amount", "target", "isIncoming", "created_at"])),
+			R.map(R.over(R.lensProp("created_at"), d => (new Date(d)).toLocaleDateString("ru-RU")))
 		)(_operations);
 
 		return ctx.render({
 			name,
 			address: keypair.publicKey(),
-			transactions,
+			operations: operations,
 		});
 	}),
 };
 
-export default function Transactions({ name, address, transactions }) {
+export default function Transactions({ data: { name, address, operations } }) {
 	return dashboardScreen(
-		<article>
-			<div class="row gap-x-4 mb-1">
-				<div class="flex items-center text-4">
-					ОТ <input type="text" value="08/12/2022" class="w-36 mx-1 px-6 py-2.5" />
-					<img src={asset("/./calendar.png")} />
+		<article class="flex flex-col">
+			<div class="lg:hidden flex justify-between font-bold text-sm pb-2">
+					Все транзакции<span class="text-[10px] font-normal">
+						Всего <span class="text-xs font-bold"> 2345</span>
+					</span>
 				</div>
-				<div class="flex items-center">
-					ДО <input type="text" value="24/02/23" class="w-32 mx-1 px-6 py-2.5" />
-					<img src={asset("/./calendar.png")} />
+			<div class="row gap-x-4 mb-4">
+				<div class="flex items-center lg:text-base text-sm">
+					ОТ <input type="text" value="08/12/2022" class="lg:w-36 w-[92px] lg:ml-1 lg:mr-2 lg:px-6 py-2.5 text-xs lg:text-lg lg:font-medium px-3 mx-3 rounded-sm" />
+					<img src={asset("/./calendar.png")} class='w-4 lg:w-6'/>
+				</div>
+				<div class="flex items-center lg:text-base text-sm">
+					ДО <input type="text" value="24/02/23" class="lg:w-36 w-[92px] lg:ml-1 lg:mr-2 lg:px-6 py-2.5 text-xs lg:text-lg lg:font-medium px-3 mx-3 rounded-sm" />
+					<img src={asset("/./calendar.png")} class='w-4 lg:w-6'/>
 				</div>
 			</div>
-			<div class="flex items-center pr-10 gap-x-6">
-				<span class="text-black">ВСЕ</span>
-				<span class="text-gray-main">ИСХОДЯЩИЕ</span>
-				<span class="text-gray-main">ВХОДЯЩИЕ</span>
-				<div class="flex gap-x-3 items-center ml-auto text-black">
+			<div class="flex items-center lg:text-base lg:pr-10 lg:gap-x-6 border-b pb-1 border-[#D1D1D1] lg:border-none mb-2 text-sm">
+				<span class="text-black mr-6">ВСЕ </span>
+				<span class="text-gray-dark mr-6">ИСХОДЯЩИЕ </span>
+				<span class="text-gray-dark">ВХОДЯЩИЕ</span>
+				<div class="lg:flex gap-x-3 items-center ml-auto text-black hidden">
 					<span class="text-4xl">2345</span>
 					Всего транзакций
 				</div>
 			</div>
-			<div class="col gap-y-3 pr-5 overflow-y-scroll scrollbar">
-				{transactions.map(Transaction)}
+			<div class="col gap-y-3 lg:pr-5 pr-0 overflow-y-scroll scrollbar">
+				<TransactionsI operations={operations} />
 			</div>
 		</article>,
 	);
