9ad05c65ebfdea2d56bc384bffb622ac637c9242
Agus <agustinrios1006@gmail.com>
2023-04-11 23:37:11 -0300

    Updated Calendar widget in the Dashboard to make it look more like the Figma

--- components/dashboard/Calendar.tsx
+++ components/dashboard/Calendar.tsx
@@ -1,54 +1,51 @@
-import { asset } from "$fresh/runtime.ts";
+import {asset} from '$fresh/runtime.ts';
 
 const dataMas = [
-	{
-		date: "24/02/23",
-		time: "11:45",
-		desc: "Окончание строительства виллы по ул. Габела, ключи будут выданы в течение месяца",
-	},
-	{
-		date: "18/02/23",
-		time: "14:05",
-		desc: "Пятьдесят восемь инвесторов получили свои первые дивиденды. Поздравляем!",
-	},
-	{
-		date: "18/02/23",
-		time: "14:05",
-		desc: "Пятьдесят восемь инвесторов получили свои первые дивиденды. Поздравляем!",
-	},
+    {
+        date: '24/02/23',
+        time: '11:45',
+        desc: 'Окончание строительства виллы по ул. Габела, ключи будут выданы в течение месяца',
+    },
+    {
+        date: '18/02/23',
+        time: '14:05',
+        desc: 'Пятьдесят восемь инвесторов получили свои первые дивиденды. Поздравляем!',
+    },
+    {
+        date: '18/02/23',
+        time: '14:05',
+        desc: 'Пятьдесят восемь инвесторов получили свои первые дивиденды. Поздравляем!',
+    },
 ];
 export default function CalendarComponent() {
-	return (
-		<div class="bg-white-light w-full h-full">
-			<div class="flex justify-start items-center pl-6 pt-3">
-				<span class="text-lg font-medium">Календарь событий</span>
-			</div>
-			<div class="flex justify-center" style={{ flex: 0.5 }}>
-				<div class="w-[86%] h-[1px] bg-gray-dashed" />
-			</div>
-			<div class="overflow-scroll" style={{ flex: 2, height: "45%" }}>
-				{dataMas.map(CalendarEvent)}
-			</div>
-			<div class="flex justify-center mt-1" style={{ flex: 0.5 }}>
-				<div class="w-[86%] h-[1px] bg-gray-dashed" />
-			</div>
-			<div class="flex justify-center items-center" style={{ flex: 1 }}>
-				<img src={asset("/Calendar.png")} style={{ width: "45%", height: "45%" }} />
-			</div>
-		</div>
-	);
+    return (
+        <div class="bg-white-light w-full h-full p-7">
+            <span class="text-xl font-bold">
+                Календарь событий
+            </span>
+            <hr class="text-gray-dashed" />
+            <div class="overflow-scroll h-2/5 shadow-md">
+                {dataMas.map(CalendarEvent)}
+            </div>
+            <hr class="text-gray-dashed" />
+            <div class="flex justify-center items-center">
+                <img
+                    src={asset('/Calendar.png')}
+                    class="w-3/5"
+                />
+            </div>
+        </div>
+    );
 }
 
-function CalendarEvent({ date, time, desc }) {
-	return (
-		<div class="w-full h-[45%] bg-gray-white-light mt-2 items-center flex flex-col justify-center items-center pl-3 pr-3">
-			<div class="flex flex-1 justify-between w-full items-center">
-				<span>{date}</span>
-				<span>{time}</span>
-			</div>
-			<div class="flex flex-1 items-center">
-				<span class="text-xs text-gray-dashed">{desc}</span>
-			</div>
-		</div>
-	);
+function CalendarEvent({date, time, desc}) {
+    return (
+        <div class="w-full bg-gray-light text-xs p-2 my-1">
+            <div class="flex justify-between font-bold">
+                <span>{date}</span>
+                <span>{time}</span>
+            </div>
+            <span>{desc}</span>
+        </div>
+    );
 }
